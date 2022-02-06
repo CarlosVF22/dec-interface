39ae39b366815bd31607a83d6bc54938774d5b71
CarlosVF22 <cevasquezf22@gmail.com>
2022-02-06 10:45:30 -0500

    visualización de jsonrpc desde ek frontend utilizando react y web3

---
+++ src/views/home/skeleton.js
@@ -0,0 +1,30 @@
+import { useWeb3React } from "@web3-react/core";
+import { useCallback, useEffect, useState } from "react";
+import usePlatziPunks from "../../hooks/usePlatziPunks";
+
+const Home = () => {
+    const { active } = useWeb3React();
+    const [maxSupply, setMaxSupply] = useState();
+
+    const platziPunks = usePlatziPunks();
+
+    const getMaxSupply = useCallback(async () => {
+        if(platziPunks) {
+            const result = await platziPunks.methods.maxSupply().call();
+            setMaxSupply(result);
+        }
+    }, [platziPunks]);
+
+    useEffect(() => {
+        getMaxSupply();
+    }, [getMaxSupply])
+
+    if (!active) return "conecta tu wallet"
+    return (
+        <>
+            <p>max supply: {maxSupply}</p>
+        </>
+    );
+};
+
+export default Home;
